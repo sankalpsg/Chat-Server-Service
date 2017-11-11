ad2dd4ba2b095a90fba85fa6909a9f53eba2f48e
Sankalp <Sankalp@192.168.0.248>
2017-11-11 10:32:24 +0000

    HashMaps

--- src/ScalableComputing/Data.java
+++ src/ScalableComputing/Data.java
@@ -2,12 +2,17 @@ package ScalableComputing;
 
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.Set;
 import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Data {
 
 	static  HashMap<String,Integer> chatRooms=new HashMap<String,Integer>();  //Stores  ChatRoom ID and Name 
+	static  HashMap<Integer,String> chatRoomsInverse=new HashMap<Integer,String>();  //Inverse of the above HashMap
 	static int chatRoomsIndex=0;											  //Count of Chat Rooms
-	static HashMap<Integer,PrintStream> writers = new HashMap<Integer,PrintStream>();	//Client ids O/P Stream.
-	static HashMap<Integer,Integer> clients = new HashMap<Integer,Integer>();	//Client ID and Chat room Relationship
+	static ConcurrentHashMap<Integer,PrintStream> writers = new ConcurrentHashMap<Integer,PrintStream>();	//Client ids O/P Stream.
+	static ConcurrentHashMap<Integer,Integer> clients = new ConcurrentHashMap<Integer,Integer>();	//Client ID and Chat room Relationship
+	static HashMap<String,Set<Integer>> clientNames = new HashMap<String,Set<Integer>>();		//All the Names of the clients and their ID
+	static int count=0;
 }
